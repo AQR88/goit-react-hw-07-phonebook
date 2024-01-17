9fb0c90942738d7c5127b90e513673d69dddec9d
AQR88 <timahaua@gmail.com>
2024-01-17 20:51:35 +0200

    deleting_problem

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -14,29 +14,7 @@ const contactsSlice = createSlice({
       state.filter = action.payload;
     },
   },
-  // extraReducers: builder => {
-  //   builder
-  //     .addCase(fetchContacts.pending, state => {
-  //       state.isLoading = true;
-  //       state.error = null;
-  //     })
-  //     .addCase(fetchContacts.fulfilled, (state, action) => {
-  //       state.isLoading = false;
-  //       state.items = action.payload;
-  //     })
-  //     .addCase(fetchContacts.rejected, (state, action) => {
-  //       state.isLoading = false;
-  //       state.error = action.error.message;
-  //     })
-  //     .addCase(addContacts.fulfilled, (state, action) => {
-  //       state.items.push(action.payload);
-  //     })
-  //     .addCase(deleteContacts.fulfilled, (state, action) => {
-  //       state.items = state.items.filter(
-  //         contact => contact.id !== action.payload
-  //       );
-  //     });
-  // },
+
   extraReducers: builder => {
     builder
       .addCase(fetchContacts.pending, state => {
